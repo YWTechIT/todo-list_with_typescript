dc115a8260be4c012b6a8a17133b70fbebb96bdb
YWTechIT <abcd8637@naver.com>
2021-07-12 15:48:39 +0900

    FIX App.tsx for base-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,25 @@
-import React, { useState } from "react";
-import TodoListItem from "./components/TodoListItem";
+import React from "react";
+import TodoItemBox from "./components/TodoItemBox";
+import {Todo} from './components/TodoItem';
 
-interface Todo {
-  text: string;
-  check: boolean;
-};
-
-const initialObj: Array<Todo> = [
+const TODO_CONSTANT: Todo[] = [
   {
-    text: "수영하자",
-    check: true,
+    id: 1,
+    text: '타입스크립트',
+    done: true
   },
   {
-    text: "헬스하자",
-    check: false,
+    id: 2,
+    text: '파이썬 알고리즘',
+    done: false
   },
-];
+]
 
-const App: React.FunctionComponent = () => {
-  const [toggles, setToggle] = useState(initialObj);  
-
-  const getChangeToggle = (toggleObj: Todo) => {
-    const newToggle = toggles.map((toggle) => {
-      if (toggle === toggleObj){
-        return {
-          ...toggle,
-          check: !toggle.check
-        }
-      }
-      return toggle
-    })
-    setToggle(newToggle);
-  }
-
+const App:React.FunctionComponent = () => {
   return (
     <>
-      <TodoListItem todo={initialObj[0]} getChangeName = {getChangeToggle}/>
-      <TodoListItem todo={initialObj[1]} getChangeName = {getChangeToggle} />
+      <div>안되면 될 때까지 시도하기 U can do it!</div>
+      <TodoItemBox todos={TODO_CONSTANT}></TodoItemBox>
     </>
   );
 };
