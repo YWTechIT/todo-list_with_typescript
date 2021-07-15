04b5e830cb6fe9aea2d746396b4926559f71a603
YWTechIT <abcd8637@naver.com>
2021-07-15 14:47:25 +0900

    ADD input창에 값 입력 후 버튼 누르면 state 변경하기
    1. input창에 원하는 내용을 친다.
      * 이때, e.target.value의 값이 바뀌게 된다.
    2. `추가` 버튼을 누른다
      * 기존 todo객체를 담고있는 state를 복사해온다음 push로 현재 값을 추가해준다.

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,71 @@ const TODO_CONSTANT: TodoType[] = [
 
 const App: React.FunctionComponent = () => {
   const [todos, setTodos] = useState<TodoType[]>(TODO_CONSTANT);
+  const [inputTitle, setInputTitle] = useState<string>("");
+  const [todoId, setTodoId] = useState<number>(todos.length);
+
   const getToggleState = (id: string) => {
     const newTodo = todos.map((todo) =>
       todo.id === id ? { ...todo, done: !todo.done } : todo
     );
-    console.log(` 변경된 state = ${id}`)
+    console.log(` 변경된 state = ${id}`);
     setTodos(newTodo);
   };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputTitle(e.target.value);
+  };
+
+  const handleClick = () => {
+    let currentTodo: TodoType[] = [...todos];
+    let currentId: number = todoId + 1;
+
+    if (inputTitle === "") {
+      alert("내용을 입력해주세요!");
+    } else {
+      const newTodo = {
+        id: String(currentId),
+        text: inputTitle,
+        done: false,
+      };
+
+      currentTodo.push(newTodo);
+      setTodos(currentTodo);
+      setTodoId(currentId);
+      setInputTitle("");
+      console.log(currentTodo);
+    }
+  };
+
+  const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  };
+
   return (
     <>
-      <div>안되면 될 때까지 시도하기 U can do it!</div>
-      <div>2주차 - getToggle 함수 생성</div>
-      <TodoItemBox todos={todos} getToggleState={getToggleState}></TodoItemBox>
+      <ul>
+        <h1> ❏ 안되면 될 때까지 시도하기 U can do it!</h1>
+        <h2> ☉ 3주차 - input 내용 입력 후 버튼 클릭하면 목록 추가하기</h2>
+      </ul>
+
+      <div>
+        <ul>
+          <li>
+            <input
+              onChange={onChange}
+              value={inputTitle}
+              onKeyPress={onKeyPress}
+            />
+            <button onClick={()=>handleClick}>추가</button>
+          </li>
+        </ul>
+        <TodoItemBox
+          todos={todos}
+          getToggleState={getToggleState}
+        ></TodoItemBox>
+      </div>
     </>
   );
 };
