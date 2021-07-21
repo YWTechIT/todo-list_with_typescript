c154418ca6d63ce0a1199472e3831a54a66572a1
YWTechIT <abcd8637@naver.com>
2021-07-21 14:47:30 +0900

    ADD TODO_CONTSTANT.ts for save CONSTANT obj
    CONSTANT_TODO 저장을 위한 파일 생성

---
+++ src/TODO_CONSTANT.ts
@@ -0,0 +1,16 @@
+import { Todo } from "../src/components/TodoItem";
+
+const TODO_CONSTANT: Todo[] = [
+  {
+    id: 1,
+    text: "타입스크립트",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "파이썬 알고리즘",
+    done: false,
+  },
+];
+
+export default TODO_CONSTANT;
