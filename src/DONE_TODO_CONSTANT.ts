5064d0cba7f9d2a611ad23334c1a0909faf5a5bd
YWTechIT <abcd8637@naver.com>
2021-07-21 15:33:21 +0900

    ADD DONE_TODO_CONSTANT, change 변수 값
    완료한 목록을 추가하는 변수추가 및 기존 todo_constant 객체 값 변경

---
+++ src/DONE_TODO_CONSTANT.ts
@@ -0,0 +1,11 @@
+import { Todo } from "../src/components/TodoItem";
+
+const DONE_TODO_CONSTANT: Todo[] = [
+  {
+    id: "1",
+    text: "수영하기",
+    done: true,
+  },
+];
+
+export default DONE_TODO_CONSTANT;
--- src/TODO_CONSTANT.ts
+++ src/TODO_CONSTANT.ts
@@ -2,13 +2,18 @@ import { Todo } from "../src/components/TodoItem";
 
 const TODO_CONSTANT: Todo[] = [
   {
-    id: 1,
-    text: "타입스크립트",
-    done: true,
+    id: "1",
+    text: "TS 공부하기",
+    done: false,
   },
   {
-    id: 2,
-    text: "파이썬 알고리즘",
+    id: "2",
+    text: "JS 알고리즘 공부하기",
+    done: false,
+  },
+  {
+    id: "3",
+    text: "독서하기",
     done: false,
   },
 ];
